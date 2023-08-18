418c243f802f134cdefe8c4d55da3ba12ee716e3
Eduardo-Vega <eduvega015@gmail.com>
2023-08-18 12:19:39 -0600

    RandTotalSum - output format

--- 2Darray/RandTotalSumArray.c
+++ 2Darray/RandTotalSumArray.c
@@ -7,9 +7,8 @@
         Create an 3x4 array with integer numbers between 15 and 55
         to calc rows and columns totals.
 ********************************************************************/
-
-#define N 3
-#define M 4
+#define N 12
+#define M 13
 
 //generating random number
 int randNum(int min, int max){
@@ -25,16 +24,20 @@ void randArrInit(int n, int m, int arr[n][m]){
 //get rows array total sum 
 void TotalSumArray(int n, int m, int arr[n][m]){
     int total = 0;
+    printf("%*s   Total\n", m*4, "");
+
     //go to through 2D array
     for (int i = 0; i < n*m; i++) {
 
         total += arr[i / m][i % m];
-        printf("\t%2d", arr[i / m][i % m]);
+        printf("%4d", arr[i / m][i % m]);
+        
         //line jump + sum result
         if ((i+1) % m == 0) { 
-            printf("\t%2d\n", total);
+            printf(" |%6d\n", total);
             total = 0; 
         }
+
     }
 }
 
@@ -46,20 +49,19 @@ void main()
     int sumf[M] = {0};
     int sumc[M] = {0};
     char reset;
+
     //generatng truly rand numbers
     srand(time(NULL));
 
     do
     {
         
-        printf("\n---RANDOM ARRAY GENERATED BETWEEN 15 N 50---\n");
-        printf("\n\t---------------------------    Total\n");
+        printf("\n---RANDOM ARRAY GENERATED BETWEEN 15 AND 50---\n");
         //init rand array
         randArrInit(N, M, array);
         //calc total sum
         TotalSumArray(N, M, array);
 
-        printf("\t---------------------------\n");
         printf("Reset? [y]/[n]: ");
         scanf(" %c", &reset);
         system("CLS");
